7528f8188b7b48101d5571dbb34de8859715aa82
mitalivaghela1515 <158211579+mitalivaghela1515@users.noreply.github.com>
2024-02-06 15:45:46 -0600

    Create mergesort.py

---
+++ mergesort.py
@@ -0,0 +1,35 @@
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2  # Finding the mid of the array
+        L = arr[:mid]       # Dividing the array elements into 2 halves
+        R = arr[mid:]
+
+        merge_sort(L)       # Sorting the first half
+        merge_sort(R)       # Sorting the second half
+
+        i = j = k = 0
+
+        # Copy data to temp arrays L[] and R[]
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+
+        # Checking if any element was left
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
+
+arr = [5,2,4,7,1,3,2,6]
+merge_sort(arr)
+print("Sorted array is:", arr)
